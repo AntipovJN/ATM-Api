efe25f0b14246a227b06203bd8a3f3b7dbdb245a
atm-test <atm.mate.test@gmail.com>
2019-10-03 00:32:27 +0300

    remove

--- src/main/java/com/atm/test/demo/repository/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.atm.test.demo.repository;
-
-import com.atm.test.demo.entity.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User, Long> {}
